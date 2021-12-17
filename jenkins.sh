4a43b45ef88a73a8a56da83f59a5e3171155553c
sravan-github <39382490+sravan-github@users.noreply.github.com>
2021-12-17 17:54:24 +0530

    Create jenkins.sh

---
+++ jenkins.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo apt update
+sudo apt install openjdk-8-jdk -y
+wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt update
+sudo apt install jenkins -y
+systemctl status jenkins
